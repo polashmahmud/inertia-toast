ac10ca18b6748388658e4794201ce2e7556e21c3
Polash Mahmud <polashmahmud@gmail.com>
2025-10-27 17:41:19 +0600

    Add notification handling in InertiaToast plugin

---
+++ resources/js/plugins/notification.ts
@@ -0,0 +1,18 @@
+import { router, usePage } from "@inertiajs/vue3";
+import { toast } from "vue-sonner";
+
+export const notification = () => {
+    router.on('finish', () => {
+        const notification = usePage().props.notification as { type?: string; body?: string };
+
+        if (notification?.body && notification?.type) {
+            switch (notification.type) {
+                case 'success': toast.success(notification.body); break;
+                case 'error': toast.error(notification.body); break;
+                case 'warning': toast.warning(notification.body); break;
+                case 'info': toast.info(notification.body); break;
+                default: toast(notification.body);
+            }
+        }
+    });
+};
