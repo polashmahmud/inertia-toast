87890a2fbe729e4858584307bbf1bbd16fd2538a
Polash Mahmud <polashmahmud@gmail.com>
2025-10-27 17:34:30 +0600

    Initialize InertiaToast package with service provider and placeholder files

---
+++ src/InertiaToastServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PolashMahmud\InertiaToast;
+
+use Illuminate\Support\ServiceProvider;
+
+class InertiaToastServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
